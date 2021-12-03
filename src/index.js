7e073f334ae2ff81649d3f46c38df97cced0c81e
xxljunjun <793117268@qq.com>
2021-12-03 14:12:00 +0800

    初始化页面UI

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import snake from "./snake.css";
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin"); //关联js和html
+module.exports = {
+  mode: "development",
+  //Using source maps
+  // devtool: 'inline-source-map',
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  //项目入口
+  entry: {
+    app: "./src/index.js",
+  },
+  //项目出口
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  //插件
+  plugins: [
+    //该snake游戏暂时不需要自动生成html
+    // new HtmlWebpackPlugin({
+    //   title: "Output Management",
+    // }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
